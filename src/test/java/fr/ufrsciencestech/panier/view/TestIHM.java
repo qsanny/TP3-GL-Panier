7b318152fc6c0c7857c8f8bcde556454d79687b0
Daniel Pinson <dp065247@MI105-15.iem>
2022-10-11 10:00:33 +0200

    debut test ihm

---
+++ src/test/java/fr/ufrsciencestech/panier/view/TestIHM.java
@@ -0,0 +1,73 @@
+package fr.ufrsciencestech.panier.view;
+
+//import fr.ufrsciencestech.panier.controler.Controleur;
+import fr.ufrsciencestech.panier.view.VueGraphiqueListe;
+
+import org.junit.Test;
+import org.junit.Before;
+
+
+public class TestIHM {
+    VueGraphiqueListe view;
+
+    @Before
+    public void setup(){
+        view = new VueGraphiqueListe();
+    }
+
+    /*
+    @Test
+    public void update(){
+        view.update()
+    }
+
+    @Test
+    public void testAjoutControleur(ControleurSimple c){
+    }
+
+    @Test
+    public void testGetInc(){
+        view.getInc();
+        assertEquals(view.getAffiche(), 1);
+    }
+
+    @Test
+    public void testGetDec0(){
+        view.getDec();
+        asserEquals(view.getAffiche(), 0); // Impossible de décrémenter à partir de 0
+    }
+
+    @Test
+    public void testGetDec1(){
+        view.getInc();
+        view.getDec();
+        asserEquals(view.getAffiche(), 0);
+    }
+
+    @Test
+    public void testGetAffiche(){
+        view.setAffiche() = "Test";
+        assertEquals(view.getAffiche(), "Test");
+    }
+
+    @Test
+    public void testSetAffiche(){
+
+    }
+
+    @Test
+    public void testGetListeFruit(){
+
+    }
+
+    @Test
+    public void testSetListeFruit(){
+
+    }
+
+    @Test
+    public void ajouteFruit(){
+        view.ajouteFruit(newOrangjavae);
+    }
+    */
+}
